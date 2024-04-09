0277b22f90aa47b0f67f304f1cc40dc3f1d1b228
r3tiu <tbp@sv-tel.ru>
2024-04-09 10:53:40 +0300

    +

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@ fn main(){
     let a = summa();
     println!("Summ is: {a}");
     if_else(5);
+    some_range();
+    some_elements();
 }
 
 fn get_local_time(){
@@ -27,9 +29,32 @@ fn summa() -> i32{
 }
 
 fn if_else(number: i32){
-    if number <= 5{
-        println!("condition was true");
-    } else{
-        println!("condition was false");
+    if number > 5{
+        println!("More then five");
+    } else if number == 5 {
+        println!("Bro, low five!");
+    } else if number < 5 {
+        println!("Less then 5")
+    } else {
+        println!("Something another, dude")
+    }
+}
+
+fn some_range(){
+    let mut a: i32 = 0;
+    for i in 1..5{
+        println!("Now \"i\" is : {} and \"a\" is : {}", i, a);
+        a += i
+    }
+}
+
+fn some_elements(){
+    let a = [10,20,30,40,50];
+
+    let b = a.len();
+    println!("Len of array is: {b}");
+
+    for el in a {
+        println!("the value is: {el}");
     }
 }
